bb3716ed6f0a5085cc34d1ac8ccecf7e022fdbab
Vitalij Ryndin <sys@crashmax.ru>
2021-11-16 14:04:11 +0800

    сustom ыcrollbar

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+const scrollbar = {
+  '&::-webkit-scrollbar': {
+    width: '4px'
+  },
+  '&::-webkit-scrollbar-thumb': {
+    background: mode('gray.300', 'gray.600'),
+    borderRadius: '24px'
+  }
+};
 
 export const theme = extendTheme({
   config: {
@@ -7,6 +18,7 @@ export const theme = extendTheme({
   },
   styles: {
     global: {
+      ...scrollbar,
       body: {
         fontSize: '14px'
       }
