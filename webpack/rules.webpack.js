ae1d3ef8ef4c015c6eb68d8700bd685e5319ac5c
Max Diachenko <maqsim@mail.com>
2022-05-11 03:22:52 +0300

    add icon

--- webpack/rules.webpack.js
+++ webpack/rules.webpack.js
@@ -7,7 +7,7 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      loader: '@vercel/webpack-asset-relocator-loader',
       options: {
         outputAssetBase: 'native_modules',
       },
